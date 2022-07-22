94bc7eac5f164252c83f86294f5850f28f096f8e
sovs <sobha.bisoi@gmail.com>
2022-07-22 15:50:11 +0530

    jenkins and docker file changed

--- src/test/java/createLoanPage.java
+++ src/test/java/createLoanPage.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.remote.tracing.Status;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -11,40 +13,41 @@ import utiliy.CoreBrowser;
 
 import static java.lang.System.in;
 
+
 public class createLoanPage extends CoreBrowser {
 
-    @BeforeMethod
-    public void init() {
-        CoreBrowser.browser();
-        CoreBrowser.setUrl();
-    }
-
-    @Test(priority = 1, enabled = true)
-    public void launchUrl() {
-
-
-        ExtentTest logger = extent.createTest("Open Google");
-        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
-
-        CoreBrowser.setUrl();
-        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
-    }
-
-    @Test(priority = 2, enabled = true)
-    public void trackLoan() {
-        ExtentTest logger = extent.createTest("Track Loan");
-        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
-
-        CoreBrowser.setUrl();
-        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
-    }
-
-    @Test(priority = 3, enabled = true)
-    public void updateBrokerDetails() {
-        ExtentTest logger = extent.createTest("Update Broker Details");
-        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
-
-        CoreBrowser.setUrl();
-        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
-    }
+//    @BeforeMethod
+//    public void init() {
+//        CoreBrowser.browser();
+//        CoreBrowser.setUrl();
+//    }
+//
+//    @Test(singleThreaded = true,timeOut = 5,enabled = false, alwaysRun = true,  invocationCount = 10)
+//    public void launchUrl() {
+//
+//
+//        ExtentTest logger = extent.createTest("Open Google");
+//        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
+//
+//        CoreBrowser.setUrl();
+//        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
+//    }
+//
+//    @Test(priority = 2, enabled = true)
+//    public void trackLoan() {
+//        ExtentTest logger = extent.createTest("Track Loan");
+//        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
+//
+//        CoreBrowser.setUrl();
+//        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
+//    }
+//
+//    @Test(priority = 3, enabled = true)
+//    public void updateBrokerDetails() {
+//        ExtentTest logger = extent.createTest("Update Broker Details");
+//        logger.log(com.aventstack.extentreports.Status.PASS, "Browser Launched");
+//
+//        CoreBrowser.setUrl();
+//        logger.log(com.aventstack.extentreports.Status.PASS, "URL Launched");
+//    }
 }
